0a30a42380afff760d6f923a5d60abf66e99bd68
eumji025 <eumji025@gmail.com>
2018-07-12 13:03:17 +0800

    完善demo

--- eutil-xstrem-plus/src/test/java/com/eumji/xstream/test/UserXstreamTest.java
+++ eutil-xstrem-plus/src/test/java/com/eumji/xstream/test/UserXstreamTest.java
@@ -1,5 +1,7 @@
 package com.eumji.xstream.test;
 
+import com.alibaba.fastjson.JSON;
+import com.eumji.xstream.model.Person;
 import com.eumji.xstream.model.User;
 import com.eumji.xstream.util.XstreamBuildFactory;
 import com.thoughtworks.xstream.XStream;
@@ -7,8 +9,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+
 /**
- * @description: write some thing of this file
+ * @description: todo 去做几个序列化的效率对比
  * @email eumji025@gmail.com
  * @author: EumJi
  * @date: 2018-07-01
@@ -25,7 +29,7 @@ public class UserXstreamTest {
      */
     @Test
     public void commonXstreamTest(){
-        XStream xStream = XstreamBuildFactory.buildDefaultXstream();
+        XStream xStream = XstreamBuildFactory.buildXstreamByKey("demoXstream");
         User user = new User();
         user.setAddress("海天一路");
         user.setName("张三");
@@ -33,6 +37,78 @@ public class UserXstreamTest {
         String value = xStream.toXML(user);
         logger.info("{}.{} method the xstream format value is :{}",UserXstreamTest.class.getCanonicalName(),"commonXstreamTest",value);
 
+    }
+
+    @Test
+    public void listTest(){
+        //language=XML 同学
+        String xml = "<com.eumji.xstream.model.Person>\n" +
+                "  <name>grand</name>\n" +
+                "  <address>shenzhen</address>\n" +
+                "  <age>18</age>\n" +
+                "  <relationship>\n" +
+                "    <com.eumji.xstream.model.Person_-Relationship>\n" +
+                "      <name>lisi</name>\n" +
+                "      <description>good partner</description>\n" +
+                "    </com.eumji.xstream.model.Person_-Relationship>\n" +
+                "  </relationship>\n" +
+                "</com.eumji.xstream.model.Person>";
+
+        XStream defaultXStream = XstreamBuildFactory.createDefaultXStream();
+        defaultXStream.alias("Person",Person.class);
+        defaultXStream.alias("relationship",Person.Relationship.class);
+        Person person = (Person) defaultXStream.fromXML(xml);
+        System.out.println(person);
+    }
+
+    /**
+     * 内部类的名称比较奇怪 com.eumji.xstream.model.Person_-Relationship
+     */
+    @Test
+    public void listToXML(){
+        Person person = initPerson();
+
+        XStream defaultXStream = XstreamBuildFactory.createDefaultXStream();
+        String s = defaultXStream.toXML(person);
+        System.out.println(s);
+    }
+
+    @Test
+    public void callback(){
+        XStream stream = XstreamBuildFactory.createXstreamAndExpand("hello-callback-expand", xStream -> {
+            xStream.alias("person", Person.class);
+            xStream.alias("relationship", Person.Relationship.class);
+            return xStream;
+        });
+
+        Person person = initPerson();
+        String s = stream.toXML(person);
+        logger.info("object to xml value is:{}",s);
+
+        Person personCopy = (Person) stream.fromXML(s);
+
+        logger.info("xml to object value is:{}",JSON.toJSONString(personCopy));
+
+
+    }
+
+    public static Person initPerson(){
+        Person person = new Person();
+        person.setName("grand");
+        person.setAddress("shenzhen");
+        person.setAge(18);
+        ArrayList<Person.Relationship> relationships = new ArrayList<>();
+        person.setRelationship(relationships);
+        Person.Relationship relationship = new Person.Relationship();
+        relationship.setName("lisi");
+        relationship.setDescription("good partner");
+        relationships.add(relationship);
+
+        Person.Relationship relationship2 = new Person.Relationship();
+        relationship2.setName("wanger");
+        relationship2.setDescription("collage classmate");
+        relationships.add(relationship2);
+        return person;
     }
 
 }
